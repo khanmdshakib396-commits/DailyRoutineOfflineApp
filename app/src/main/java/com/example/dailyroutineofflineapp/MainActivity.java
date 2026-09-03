6451547e9f0cddfa12682301cfcd713ed4b2f0d3
khanmdshakib396-commits <khanmdshakib396@gmail.com>
2026-09-03 22:05:13 +0600

    Create MainActivity.java

---
+++ app/src/main/java/com/example/dailyroutineofflineapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dailyroutineofflineapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("Daily Routine");
+        textView.setTextSize(28);
+        textView.setPadding(40, 80, 40, 40);
+
+        setContentView(textView);
+    }
+}
