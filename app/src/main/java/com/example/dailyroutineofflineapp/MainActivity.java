f4104a642dbd2c6d9bec137406b43ce54bdd7edc
khanmdshakib396-commits <khanmdshakib396@gmail.com>
2026-09-03 23:56:13 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/dailyroutineofflineapp/MainActivity.java
+++ app/src/main/java/com/example/dailyroutineofflineapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.dailyroutineofflineapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+
+            String user = username.getText().toString().trim();
+            String pass = password.getText().toString();
+
+            if (user.equals("Md Moyen Khan Shakib") && pass.equals("425264")) {
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
